7fa112f0baccda11ac0f96912c98d8bb3c3d9c4a
seiya <seiyaichi1999leb@gmail.com>
2023-01-14 14:47:14 +0900

    [fix]@IgnoredOnParcelの追加
    Warning対策

--- app/src/main/java/com/androidtechmanageapp/model/TechAndURL.kt
+++ app/src/main/java/com/androidtechmanageapp/model/TechAndURL.kt
@@ -3,14 +3,17 @@ package com.androidtechmanageapp.model
 import android.os.Parcelable
 import androidx.room.Embedded
 import androidx.room.Relation
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 //TechクラスとURLクラスの中間クラス
 @Parcelize
 class TechAndURL : Parcelable {
+    @IgnoredOnParcel
     @Embedded
     lateinit var tech: Tech
 
+    @IgnoredOnParcel
     @Relation(parentColumn = "id", entityColumn = "tech_id")
     lateinit var urls: List<URL>
 }
